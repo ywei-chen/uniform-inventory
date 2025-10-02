d92ac3f4afc4049b3b6ca64148be9b3a68cbc619
Kane C.(陳奕瑋) <F19712@pwind.com.tw>
2025-10-02 17:16:38 +0800

    20251002_1706

--- models/uniformInfo.js
+++ models/uniformInfo.js
@@ -1,2 +1,16 @@
 //定義MongoDB的Schema
+import mongoose from "mongoose";
+
+
+const uniformInfoSchema = new mongoose.Schema({
+    code: {type: String, required: true},
+    gender: {type: String, required: true},
+    sizes: {type: String, required: true},
+    uniform: {type: String, required: true},
+
+},{
+    timestamps: true
+});
+
+export default mongoose.models.uniformInfo || mongoose.model('uniformInfo', uniformInfoSchema);
 
